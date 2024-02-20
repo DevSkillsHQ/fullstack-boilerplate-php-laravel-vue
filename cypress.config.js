36744f4fdf5cb53f51d9d2f26768ee21c6bb7186
Ehsan Mirzarazi <ehsanmirzarazi@ehsans-mbp.home>
2024-02-20 13:23:45 +0100

    fix: cypress updated to 12.17.1

---
+++ cypress.config.js
@@ -0,0 +1,12 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  env: {
+    apiUrl: 'http://localhost:8000/api',
+  },
+  e2e: {
+    supportFile: false,
+    setupNodeEvents(on, config) {},
+    baseUrl: 'http://localhost:5000',
+  },
+})
